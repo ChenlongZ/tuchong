0cd0bedb2d0a90f8b7991ec518b8d1e1150c57a9
Chenlong Zhang <chenglong.zhang@gm.com>
2017-01-20 16:30:10 -0500

    add a tuchong image crawler

--- index.ios.js
+++ index.ios.js
@@ -9,25 +9,81 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View, 
+  Image
 } from 'react-native';
 
+const ScrollableTabView = require("react-native-scrollable-tab-view");
+import ScrollableTabBar from 'react-native-scrollable-tab-view/ScrollableTabBar.js';
+
+const imageUrl = "https://photo.tuchong.com/267872/f/15361536.jpg";
+
 export default class tuchong extends Component {
+  constructor(props) {
+    bimagePool = {
+      "landscape": [],
+      "protrait": [],
+      "travel": [],
+      "document": [],
+      "civil": [],
+      "architecture": [],
+      "city": [],
+    };     //max length = 100 for each tag
+  } 
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ScrollableTabView renderTabBar={() => <ScrollableTabBar/>}>
+        <Landscape tabLabel="风光"/>
+        <Protrait tabLabel="人像"/>
+        <City tabLabel="城市"/>
+        <Travel tabLabel="旅行"/>
+        <Civil tabLabel="人文"/>
+        <Document tabLabel="纪实"/>
+        <Architecture tabLabel="建筑"/>
+      </ScrollableTabView>
     );
+  }
+}
+
+class Landscape extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+class Protrait extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+
+class City extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+
+class Travel extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+
+class Civil extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+
+class Architecture extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
+  }
+}
+
+class Document extends Component{
+  render() {
+    return 		<Image source={{uri: imageUrl}} style={{width:500, height:250}}/> 
   }
 }
 
